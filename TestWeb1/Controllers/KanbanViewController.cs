5da578ae6e9f2d01bcf5eba7b7c7cdbd84e34051
Black - BKP <black.bkp@gmail.com>
2021-07-02 16:16:34 +0700

    Create Kanban View

---
+++ TestWeb1/Controllers/KanbanViewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWeb1.Models;
+
+namespace TestWeb1.Controllers
+{
+    public class KanbanViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetData()
+        {
+            return Json("");
+        }
+    }
+}
